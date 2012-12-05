bccd77aa66d8fbcf2d54cd9820cf8382d9c1b26e
Sven Anders Robbestad <robbestad@gmail.com>
2012-12-05 23:27:06 +0100

    games

--- public/thumb.php
+++ public/thumb.php
@@ -22,7 +22,7 @@
 */
 define ('VERSION', '2.8.2');                                        // Version of this script 
 //Load a config file if it exists. Otherwise, use the values below
-if( file_exists(dirname(__FILE__) . '/timthumb-config.php'))    require_once('timthumb-config.php');
+if( file_exists(dirname(__FILE__) . '/resources/timthumb-config.php'))    require_once('resources/timthumb-config.php');
 if(! defined('DEBUG_ON') )             define ('DEBUG_ON', false);                // Enable debug logging to web server error log (STDERR)
 if(! defined('DEBUG_LEVEL') )             define ('DEBUG_LEVEL', 1);                // Debug level 1 is less noisy and 3 is the most noisy
 if(! defined('MEMORY_LIMIT') )             define ('MEMORY_LIMIT', '30M');                // Set PHP memory limit
@@ -114,8 +114,8 @@ if(! defined('WEBSHOT_XVFB_RUNNING') )    define ('WEBSHOT_XVFB_RUNNING', false)
 // If ALLOW_EXTERNAL is true and ALLOW_ALL_EXTERNAL_SITES is false, then external images will only be fetched from these domains and their subdomains. 
 if(! isset($ALLOWED_SITES)){
     $ALLOWED_SITES = array (
-        'www.slank.no',
-        'slank.no',
+        'www.robbestad.com',
+        'www.svenardo.com',
     );
 }
 // -------------------------------------------------------------
