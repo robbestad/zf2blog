4182d501e1d47946cadb59bd59eedb3a4fe7b83e
Sven Anders Robbestad <robbestad@gmail.com>
2012-12-05 10:06:26 +0000

    RC

---
+++ config/autoload/zfctwig.global.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'zfctwig' => array(
+	 /**
+         * If set to true disables ZF's notion of parent/child layouts in favor of
+         * Twig's inheritance model.
+         */
+        'disable_zf_model' => false 
+    ),
+);
