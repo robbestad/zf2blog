9ea04f07e4e499f6aafd161c00b1ce5e123f3b50
Sven Anders Robbestad <robbestad@gmail.com>
2013-09-18 23:16:19 +0200

    blog

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -36,12 +36,6 @@ public function indexAction()
     }
 
 
-    public function errorAction()
-    {
-        $this->view->message = "Welcome to the Zend Framework with Twig";
-
-    }
-
     public function donateAction()
     {
          return new ViewModel(array(
