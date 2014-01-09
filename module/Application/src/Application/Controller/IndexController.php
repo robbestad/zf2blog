ef5b17cf994d7255ac8576eaf9f5e02f7b37b4ca
Sven Anders Robbestad <robbestad@gmail.com>
2014-01-09 22:51:09 +0100

    blog action

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -43,7 +43,7 @@ public function indexAction()
 
         $request = new Request();
         $request->setMethod(Request::METHOD_POST);
-        $request->setUri('https://www.googleapis.com/blogger/v3/blogs/3058415513828304615&key= AIzaSyCFEB9AVhg1QrjaS372KWt5sDW0qwu9ybI');
+        $request->setUri('https://www.googleapis.com/blogger/v3/blogs/3058415513828304615&key=AIzaSyCFEB9AVhg1QrjaS372KWt5sDW0qwu9ybI');
         //$r=$request->getPost();
         $r=$request->getContent();
         var_dump($r);
